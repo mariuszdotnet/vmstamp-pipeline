a731d486d3b4794893455aaa0b30ea29472e30cf
Mariusz Kolodziej <makolo@microsoft.com>
2018-05-04 22:25:58 -0400

    removed piline file

--- terraform-init-plan-apply.sh
+++ terraform-init-plan-apply.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-cd terraform-secrets
 ls
 #cd terraform-templates/workspaces/terraform-winvm
-#terraform init
-#mv ./terraform-job/terraform.tfvars ./terraform-templates/workspaces/terraform-winvm
-#terraform plan -var-file terraform-secrets/secrets.tfvars
