af8da5aabeca78c170d841c5dc1273f635b45849
Mariusz Kolodziej <makolo@microsoft.com>
2018-05-11 23:13:56 -0400

    changed job trigger name

--- terraform-init-plan-apply.sh
+++ terraform-init-plan-apply.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ls -al
-cp terraform-job/terraform.tfvars terraform-templates/workspaces/terraform-winvm/terraform.tfvars
+cp terraform-trigger-job/terraform.tfvars terraform-templates/workspaces/terraform-winvm/terraform.tfvars
 cp terraform-secrets/secrets.tfvars terraform-templates/workspaces/terraform-winvm/secrets.tfvars
 cd terraform-templates/workspaces/terraform-winvm
 ls -al
